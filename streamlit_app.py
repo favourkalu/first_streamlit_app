66efcf0883808fa53d144994f900587478ead2e5
Favour Kalu <48354662+favourkalu@users.noreply.github.com>
2023-02-09 15:41:47 +0000

    Update streamlit_app.py
    Added a header section to display fruityvice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,9 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado'
 # view dataframe
 streamlit.dataframe(my_fruit_list)
 
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
