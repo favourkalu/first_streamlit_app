796fad0d8c37fbb2b24775373f65a2ef3a6a9e57
Favour Kalu <48354662+favourkalu@users.noreply.github.com>
2023-02-11 18:46:17 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ try:
    else:
         back_from_function = get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
-streamlit.header("The fruit load list contains:")
+#streamlit.header("The fruit load list contains:")
 
         
 #snowflake-related functions
